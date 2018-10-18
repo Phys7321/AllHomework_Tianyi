74fdf8df41dff37736b09f3a2bcf73653963fbe1
tianyizhou1222 <43151462+tianyizhou1222@users.noreply.github.com>
2018-10-18 14:57:32 -0400

    HW03 diffeq Osc FINISHED

--- forced_oscillator.m
+++ forced_oscillator.m
@@ -7,7 +7,8 @@
 % dy1/dt = y2
 % dy2/dt= -2*gamma*y2-omega0_sq*sin(y1) + A0*cos(wt)
 
-function [period,sol] = forced_oscillator(omega0, gamma, A0, w, theta0, thetadot0, grph)
+function [period,sol,A_steady,sol_steady] = forced_oscillator(...
+                        omega0, gamma, A0, w, theta0, thetadot0, grph)
 
 if nargin<=4
     error('Must input initial conditions')
@@ -30,7 +31,7 @@ T_small_angle = 2*pi/omega0;
 b = 2*m*gamma;
 
 y0 = [theta0, thetadot0];                   % Initial condition
-N = 15;                                     % Number of cycles
+N = 40;                                     % Number of cycles
 steps = 500;
 tspan = linspace(0,N*T_small_angle,steps);
 
@@ -41,22 +42,28 @@ else
     opts = odeset('events',@events,'refine',6);
     [t,y]=ode45(@f,tspan,y0,opts,omega0,gamma,A0,w);        % Stop integration when equilibrium
 end
-
 sol = [t,y];
 
 if w==0
     ind = y(:,2).*circshift(y(:,2), [-1 0]) <= 0;     % Require thetadot=0
     period = 2*mean(diff(t(ind)));                    % Find t satisifying indicator
+    A_steady = A0;
+    sol_steady = sol;
 else
-    ind = y(:,2).*circshift(y(:,2), [-1 0]) <= 0;     % Require thetadot=0
-    ind = ind(150:end);                               % Cut the first a few cycles
-    period = 2*mean(diff(t(ind))); 
+    t_steady = t(250:end);
+    y_steady = y(250:end,:);
+    sol_steady = [t_steady,y_steady];
+    ind = y_steady(:,2).*circshift(y_steady(:,2), [-1 0]) <= 0;     % Require thetadot=0
+    period = 2*mean(diff(t_steady(ind)));
+    A = abs(y_steady(ind,1));
+    A = A(1:end-2);
+    A_steady = mean(A);
 end
 
 if grph
     figure
-    plot(t,y(:,1),'b','linewidth',2);
-    title(['\theta v.s. t with \gamma = 0.5, A0 = 1, \omega = ' num2str(w)])
+    plot(t,y(:,1),'b',t,y(:,2),'r','linewidth',2);
+    title(['\theta and d\theta/dt v.s. t with \gamma = ' num2str(gamma) ', A0 = 1, \omega = ' num2str(w)])
     ylabel('\theta')
     xlabel('t(s)')
 end
